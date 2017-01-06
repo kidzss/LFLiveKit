3220488ad25647c73fd42c2244c1f74c221103df
Tony Duan <duanhai@outlook.com>
2017-01-06 15:30:53 +0800

    Revert "revert"
    This reverts commit 0df0b8781f6b2574b3b9dd69d5e7cfaf01afdcdd.

--- LFLiveKit/capture/LFVideoCapture.m
+++ LFLiveKit/capture/LFVideoCapture.m
@@ -124,7 +124,10 @@
     [[NSFileManager defaultManager] removeItemAtURL:localFileURL error:nil];
     
     //加载资源
-    [self.movieWriter startRecording];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.movieWriter startRecording];
+    });
+//    [self.movieWriter startRecording];
 }
 
 - (void)stopRecording {
