fa41193ca0228ee33fb133cf9cb538e1dfaaa3fc
Tony Duan <duanhai@outlook.com>
2017-01-06 15:46:31 +0800

    revert

--- LFLiveKit/capture/LFVideoCapture.m
+++ LFLiveKit/capture/LFVideoCapture.m
@@ -123,11 +123,8 @@
     unlink(localFileURL.absoluteString.UTF8String);
     [[NSFileManager defaultManager] removeItemAtURL:localFileURL error:nil];
     
-    //加载资源
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self.movieWriter startRecording];
-    });
-//    [self.movieWriter startRecording];
+
+    [self.movieWriter startRecording];
 }
 
 - (void)stopRecording {
